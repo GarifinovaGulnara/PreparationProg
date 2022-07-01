5adad4c7d30427551fe276925eaf233de57e3344
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-07-01 22:54:48 +0300

    Add Var9

---
+++ Var9/Service/ITextService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Var9.Service
+{
+    interface ITextService
+    {
+        public void SaveText(string text);
+        public string GetText();
+    }
+}
---
+++ Var9/Service/TextService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Var9.Service
+{
+    public class TextService : ITextService
+    {
+        public string Text { get; set; } = " ";
+        public string GetText()
+        {
+            return Text;
+        }
+
+        public void SaveText(string text)
+        {
+            Text = text;
+        }
+    }
+}
