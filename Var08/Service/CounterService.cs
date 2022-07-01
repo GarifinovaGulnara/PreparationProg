8949518e13e36c15e4d36b18f1ee53285a18cb92
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-07-01 22:21:51 +0300

    Add Var8(08)

---
+++ Var08/Service/CounterService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Var08.Service
+{
+    public class CounterService : ICounterService
+    {
+        public int Count { get; set; }
+        public void Increment()
+        {
+            Count++;
+        }
+        public int GetCount()
+        {
+            return Count;
+        }
+    }
+}
---
+++ Var08/Service/ICounterService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Var08.Service
+{
+    interface ICounterService
+    {
+        public void Increment();
+        public int GetCount();
+
+    }
+}
