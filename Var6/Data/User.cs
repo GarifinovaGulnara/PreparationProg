7ffef78d36bacc5a2126f87b3535b29f7dc13c46
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-07-01 20:30:26 +0300

    Add Var6

---
+++ Var6/Data/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Var6.Data
+{
+    public class User
+    {
+        public User(string name, string surname, DateTime dateOfAdding)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfAdding = dateOfAdding;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfAdding { get; set; }
+        public static List<User> users = new List<User>();
+        public static List<string> titles = new List<string>() { "Name", "Surname", "DateOfAdding", " " };
+
+    }
+}
